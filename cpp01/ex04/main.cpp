541d9c474b63b369521255131546d0e8dd10bb0b
Samuel Ting <sting@u82z03s03.42kl.edu.my>
2025-03-24 11:56:34 +0800

    feat: completed replace_all and replace_str function
    feat: handled all edge cases

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -22,27 +22,33 @@ std::string replace_str(const std::string& oriStr, size_t start, size_t end, con
 	
     // Check if the indices are valid
     if (start > end || end >= oriStr.length()) {
-        throw std::out_of_range("Invalid start or end indices.");
+		std::cout << "Invalid start or end indices." << std::endl;
+		return (oriStr);
     }
 	std::string	modifiedStr = oriStr.substr(0, start) + str + oriStr.substr(end + 1);
 	return (modifiedStr);
 }
 
 /*
-`npos` is constant that indicates `find()` not finding any matches
+`npos` is constant that indicates `find()` not finding any matches (value = -1)
 */
 void replaceAll(std::string& content, const std::string& s1, const std::string& s2) {
 	
 	size_t pos = 0;
+	size_t s1_length = s1.length();
+
+	if (s1_length == 0)
+		return ;
 	while ((pos = content.find(s1, pos)) != std::string::npos) {
 		content = replace_str(content, pos, pos + s1.length() - 1, s2);
-	}	
+		pos += s2.length();
+	}
 }
 
 int main(int argc, char **argv) {
 
 	if (argc != 4) {
-		std::cout << "Error: Program only takes in 3 arguments" << std::endl;
+		std::cout << "Error: Program only takes in 3 arguments, <filename> <s1> <s2>" << std::endl;
 		return (1);
 	}
 
@@ -50,25 +56,35 @@ int main(int argc, char **argv) {
 	std::string	s1 = argv[2];
 	std::string s2 = argv[3];
 
+	if (s1 == "") {
+		std::cout << "Error: s1 cannot be an empty string" << std::endl;
+		return (1);
+	}
+	if (s1 == s2) {
+		std::cout << "Error: s1 cannot be same as s2" << std::endl;
+		return (1);
+	}
 
 	std::ifstream	ifs(fileName.c_str()); // opens a input file stream to read from a specified file
 	if (!ifs) {
 		std::cerr << "Error opening file" << std::endl;
         return (1); 	
 	}
-	
+
+	// extract string from file	
 	std::string content;
 	std::string	line;
 	while (std::getline(ifs, line)) {
 		content += line + "\n";	
 	}
-
-
 	ifs.close();
 
+	if (content == "") {
+		std::cout << "File is empty. Nothing to be replaced." << std::endl;
+		return (0);
+	}
+	replaceAll(content, s1, s2);
 
 	std::ofstream ofs((fileName + ".replace").c_str());
 	ofs << content;
-
-
 }
