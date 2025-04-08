22606ec258eaa118fcc494cedc6e8205f046b801
Samuel Ting <sting@u91z03s02.42kl.edu.my>
2025-04-08 16:34:11 +0800

    feat: first draft of all implementation of each Animal, Dog, Cat class

---
+++ cpp04/ex00/inc/Dog.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 10:37:40 by sting             #+#    #+#             */
+/*   Updated: 2025/04/08 11:44:20 by sting            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include <string>
+#include "Animal.hpp"
+
+class Dog : public Animal {
+public:
+
+	// Constructor
+	Dog();
+	// Copy Constructor
+	Dog(const Dog& other);
+	// Copy Assignment Operator
+	Dog& operator=(const Dog& other);
+	// Destructor
+	~Dog();
+
+	void makeSound() const; //need to add "virtual"??
+};
+
+
+#endif
---
+++ cpp04/ex00/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 10:15:18 by sting             #+#    #+#             */
+/*   Updated: 2025/04/08 11:26:23 by sting            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../inc/Animal.hpp"
+#include "../inc/Dog.hpp"
+#include "../inc/Cat.hpp"
+
+int	main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+
+	return (0);
+}
