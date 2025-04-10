c3958663fe3e1b1051d2a75fad4e56c3a3a69a51
Samuel Ting <sting@u91z03s01.42kl.edu.my>
2025-04-10 15:37:44 +0800

    chore: added subject pdf test

--- cpp04/ex03/src/main.cpp
+++ cpp04/ex03/src/main.cpp
@@ -17,6 +17,7 @@
 #include "../inc/ICharacter.hpp"
 #include "../inc/Character.hpp"
 #include "../inc/IMateriaSource.hpp"
+#include "../inc/MateriaSource.hpp"
 
 
 #define RESET "\033[0m"
@@ -57,23 +58,36 @@ int	main()
 
 	
 	{
-		// printSection("Subject PDF tests");
-		// IMateriaSource* src = new MateriaSource();
-		// src->learnMateria(new Ice());
-		// src->learnMateria(new Cure());
-		// ICharacter* me = new Character("me");
-		// AMateria* tmp;
-		// tmp = src->createMateria("ice");
-		// me->equip(tmp);
-		// tmp = src->createMateria("cure");
-		// me->equip(tmp);
-		// ICharacter* bob = new Character("bob");
-		// me->use(0, *bob);
-		// me->use(1, *bob);
-		// delete bob;
-		// delete me;
-		// delete src;
-		// return 0;
+		printSection("Subject PDF tests");
+		IMateriaSource* src = new MateriaSource();
+		std::cout << std::endl;
+
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+		std::cout << std::endl;
+
+		ICharacter* me = new Character("me");
+		AMateria* tmp;
+		std::cout << std::endl;
+
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		std::cout << std::endl;
+
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		std::cout << std::endl;
+
+		ICharacter* bob = new Character("bob");
+		std::cout << std::endl;
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+		std::cout << std::endl;
+
+		delete bob;
+		delete me;
+		delete src;
 	}
 
 	
