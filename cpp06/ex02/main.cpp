fe52c48090333a74575e48f1e116b4cdb1a2c697
Samuel Ting <samuelting@Samuels-MacBook-Pro.local>
2025-04-22 19:45:43 +0800

    feat: identify() done for ptr parameter

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/22 18:55:13 by sting             #+#    #+#             */
+/*   Updated: 2025/04/22 19:44:01 by sting            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <typeinfo>
+#include <iostream>
+
+// It randomly instanciates A, B or C and returns the instance as a Base pointer.
+/*Base * generate(void)*/
+/*{*/
+/**/
+/*}*/
+
+//  prints the actual type of the object pointed to by p: "A", "B" or "C".
+void identify(Base* p)
+{
+	const std::type_info& ti = typeid(*p);
+	std::cout << "identify: type = \"" <<  ti.name() << "\"\n";
+}
+
+// prints the actual type of the object pointed to by p: "A", "B" or "C". Using a pointer inside this function is forbidden.
+void identify(Base& p)
+{
+	const std::type_info& ti = typeid(p);
+	std::cout << "identify: type = \"" <<  ti.name() << "\"\n";
+
+}
+
+int main(void)
+{
+	{
+		Base *p = new A();
+
+		identify(p);
+		delete p;
+	}
+	{
+		A a;
+		const Base& r = a;
+
+		identify(r);
+	}
+
+}
