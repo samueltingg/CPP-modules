8203b96a12c47c86279c1a259af3a80b83653041
Samuel Ting <samuelting@Samuels-MacBook-Pro.local>
2025-04-25 20:51:39 +0800

    feat: completed easyfind() feat: test cases done for all 3 container types

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,18 +6,24 @@
 /*   By: sting <sting@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 15:19:49 by sting             #+#    #+#             */
-/*   Updated: 2025/04/25 17:55:59 by sting            ###   ########.fr       */
+/*   Updated: 2025/04/25 20:49:15 by sting            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
 // `typename` keyword is used to indicate that T::const_iterator is a type.
 template <typename T>
-typename T::const_iteractor easyfind(const T& container, int num)
+typename T::const_iterator easyfind(const T& container, int num)
 {
 	typename T::const_iterator it;
 
 	for (it = container.begin(); it != container.end(); ++it) {
-		if (container[it] == num)
+		if (*it == num)
 			return it;
 	}
+	return container.end();
 }
+
+#endif
