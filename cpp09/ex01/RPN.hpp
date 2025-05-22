bcacf145b164a0647ecd55252cd06baff48f807d
Samuel Ting <sting@u82z03s01.42kl.edu.my>
2025-05-22 17:08:55 +0800

    feat: changed token container from 'deque' to 'list'

--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -18,7 +18,7 @@
 #define CYAN "\033[36m"
 #define GREY "\033[90m"
 
-#include <deque>
+#include <list>
 #include <stack>
 #include <string>
 #include <iostream>
@@ -78,7 +78,7 @@ public:
 
 
 private:
-	std::deque<std::string> _tokens;
+	std::list<std::string> _tokens;
 	// Default Constructor
 	RPN();
 };
